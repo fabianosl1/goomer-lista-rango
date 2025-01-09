013f091cc6f31e971c62ab83c5e30693c407a2ac
fabiano de sousa lima <fabianosl@ufrrj.br>
2025-01-09 05:10:01 -0300

    chore: move schedule tests

--- tests/schedule/domain/schedule.test.ts
+++ tests/schedule/domain/schedule.test.ts
@@ -1,21 +1,20 @@
-import { Schedule } from "@/domain/entities/schedule.entity";
-import { parserToMinutes } from "@/utils/hour";
+import { parseMinutes, Schedule } from "@/schedule/domain/schedule.entity";
 
 describe("parsing input schedule hour to minutes", () => {
 	test("when valid input then ok", () => {
 		const expected = 90;
 		const input = "01:30";
 
-		const result = parserToMinutes(input);
+		const result = parseMinutes(input);
 
 		expect(result).toBe(expected);
 	});
 
 	test("when invalid input then throw error", () => {
-		const inputs = ["1:30", "001:30", "01:0", "001:330", "0130"];
+		const inputs = ["1:30", "001:30", "01:0", "001:330", "0130", "12:377"];
 
 		for (const input of inputs) {
-			expect(() => parserToMinutes(input)).toThrow();
+			expect(() => parseMinutes(input)).toThrow();
 		}
 	});
 });
@@ -26,7 +25,7 @@ describe("entity Schedule", () => {
 		expect(result).toBeInstanceOf(Schedule);
 	});
 
-	test("when valid input then ok", () => {
+	test("when short interval then throw error", () => {
 		expect(() => new Schedule("0", "12:15", "12:00", "sat")).toThrow();
 	});
 });
