caf078674e246f32e53a9fc3c3c26f2dd9a11f96
fabiano de sousa lima <fabianosl@ufrrj.br>
2025-02-01 18:58:45 -0300

    test(restaurant): test e2e get /restaurants

--- tests/restaurant/infra/restaurant.controller.test.ts
+++ tests/restaurant/infra/restaurant.controller.test.ts
@@ -1,6 +1,9 @@
 import { app } from "@/app";
+import type { ListRestaurantResponseDto } from "@/restaurant/application/dtos/list-restaurant.dto";
+import type { RestaurantResponseDto } from "@/restaurant/application/dtos/restaurant.dto";
 import { makeCreateRestaurantDto } from "tests/helpers/restaurant-helper";
 import { makeCreateScheduleDto } from "tests/helpers/schedule-helper";
+import { createRestaurant } from "tests/restaurant/infra/helper";
 
 describe("Restaurant test controller E2E", () => {
 	test("POST /restaurants", async () => {
@@ -15,13 +18,44 @@ describe("Restaurant test controller E2E", () => {
 				"Content-Type": "application/json",
 			},
 		});
-		
+
 		expect(response.status).toBe(201);
 
-		const restaurant = await response.json()
+		const restaurant: RestaurantResponseDto = await response.json();
 
-		expect(restaurant.id).toBeDefined()
-		expect(restaurant.name).toBe(body.name)
-		expect(restaurant.schedules[0].id).toBeDefined()
+		expect(restaurant.id).toBeDefined();
+		expect(restaurant.name).toBe(body.name);
+		expect(restaurant.schedules[0].id).toBeDefined();
+	});
+
+	test("GET /restaurants", async () => {
+		await createRestaurant(makeCreateRestaurantDto());
+
+		const response = await app.request("/restaurants");
+
+		expect(response.status).toBe(200);
+
+		const restaurants: ListRestaurantResponseDto = await response.json();
+
+		expect(restaurants.length).toBeDefined();
+
+		for (const restaurant of restaurants) {
+			expect(restaurant.id).toBeDefined();
+			expect(typeof restaurant.address).toBe("string");
+			expect(restaurant.picture).toBeDefined();
+		}
+	});
+
+	test("GET /restaurants/:id", async () => {
+		const created = await createRestaurant(makeCreateRestaurantDto());
+
+		const response = await app.request(`/restaurants/${created.id}`);
+
+		expect(response.status).toBe(200);
+
+		const restaurant: RestaurantResponseDto = await response.json();
+
+		expect(restaurant.id).toBeDefined();
+		expect(restaurant).toEqual(created);
 	});
 });
